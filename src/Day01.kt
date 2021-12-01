52543b013e3b5c23855de4144fe171a849671cff
Klemens Böswirth <>
2021-12-01 21:06:05 +0100

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,27 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { line ->
+            line.toInt()
+        }.windowed(2) { (a, b) ->
+            a < b
+        }.count { it }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { line ->
+            line.toInt()
+        }.windowed(3) { window ->
+            window.sum()
+        }.windowed(2) { (a, b) ->
+            a < b
+        }.count { it }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("tests/Day01.test01")
+    test(part1(testInput), 7)
+    test(part2(testInput), 5)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("inputs/Day01")
+    println("part1: ${part1(input)}")
+    println("part2: ${part2(input)}")
 }
